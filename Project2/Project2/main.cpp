9de86457ce77261fab2cfe98719d1794f50f42b9
wei <xwei0012@student.monash.edu>
2021-09-08 21:52:21 +0800

    add cmake file

--- Project2/Project2/main.cpp
+++ Project2/Project2/main.cpp
@@ -1,13 +1,19 @@
 #include "sb7.h"
+#include <math.h>
 
 class my_application :public sb7::application
 {
 public:
 	void render(double currentTime)
 	{
-		static const GLfloat red[] = { 1.0f, 0.0f, 0.0f, 1.0f };
-		glClearBufferfv(GL_COLOR, 0, red);
+		const GLfloat color[] =
+		{
+			(float)sin(currentTime) * 0.5f + 0.5f,
+			(float)cos(currentTime) * 0.5f + 0.5f,
+			0.0f, 1.0f
+		};
+		glClearBufferfv(GL_COLOR, 0, color);
 	}
 };
 
-DECLARE_MAIN(my_application)
+DECLARE_MAIN(my_application);
